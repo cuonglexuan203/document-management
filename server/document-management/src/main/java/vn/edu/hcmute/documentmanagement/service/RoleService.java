b046a8a7d9651babb91f12577fd46d1b0a0da621
hoailamcenter <21110778@student.hcmute.edu.vn>
2024-03-20 23:27:25 +0700

    update add and delete User

--- server/document-management/src/main/java/vn/edu/hcmute/documentmanagement/service/RoleService.java
+++ server/document-management/src/main/java/vn/edu/hcmute/documentmanagement/service/RoleService.java
@@ -3,10 +3,13 @@ package vn.edu.hcmute.documentmanagement.service;
 import vn.edu.hcmute.documentmanagement.model.Role;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface RoleService {
+
     Role addRole(Role role);
     List<Role> addAllRoles(Role[] roles);
     List<Role> addAllRoles(List<Role> roles);
+    Role getRoleByIdOrElseThrow(long id, String messageError);
 
 }
