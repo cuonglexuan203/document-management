4a61778192139389266428b17d59a0d02c1564e7
Vo Dang Trinh <vodangtrinh03@gmail.com>
2024-03-21 11:07:12 +0700

    Update Upload pages

--- client/document-management/src/app/(main)/_components/Finish.tsx
+++ client/document-management/src/app/(main)/_components/Finish.tsx
@@ -6,7 +6,7 @@ const Finish = () => {
             <p className="mt-20 mt-20 text-center font-bold text-5xl">Thank you for uploading</p>
             <div className="my-24 flex justify-center items-center">
                 <button className="bg-gray-200 hover:bg-gray-300 text-black py-5 px-4 rounded mr-40 ">Upload new document</button>
-                <button className="bg-gray-200 hover:bg-gray-300 text-black py-5 px-4 rounded ">Back to Home Page</button>
+                <button className="bg-gray-200 hover:bg-gray-300 text-black py-5 px-4 rounded ">Home Page</button>
             </div>
         </div>
     )
