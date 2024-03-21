8f4f9f4de78d88524a52cb309dd9b46837ad5b84
Vo Dang Trinh <vodangtrinh03@gmail.com>
2024-03-21 08:58:51 +0700

    Update Upload page

--- client/document-management/src/app/(main)/_components/Upload.tsx
+++ client/document-management/src/app/(main)/_components/Upload.tsx
@@ -19,11 +19,11 @@ const Upload = () => {
                 </div>
             </div>
             <div className="mt-16 ml-28 flex items-center">
-                <button className="bg-gray-200 hover:bg-gray-300 text-black py-5 px-4 rounded">Upload thumbnail</button>
+                <button className="bg-gray-200 hover:bg-gray-300 text-black py-5 px-4 rounded">Browse a file</button>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="ml-5 w-6 h-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
                 </svg>
-                <p className="ml-5 text-base">Image title</p>
+                <p className="ml-5 text-base">Document title</p>
             </div>
             <div className="mt-14 mr-28 block flex justify-end">
                 <button className="bg-gray-200 hover:bg-gray-300 text-black py-5 px-6 rounded mr-5">Cancel</button>
